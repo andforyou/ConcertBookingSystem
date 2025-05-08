6bac22997a195569c1ecddd0475ff9c4e76b93e7
Jerrie Yang <jingwen.yang-1@student.uts.edu.au>
2025-05-08 15:00:57 +1000

    Initial commit of Xcode project

---
+++ ConcertBookingSystem/ConcertBookingSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcertBookingSystemApp.swift
+//  ConcertBookingSystem
+//
+//  Created by 牙后慧 on 2025/5/8.
+//
+
+import SwiftUI
+
+@main
+struct ConcertBookingSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcertBookingSystemTests/ConcertBookingSystemTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConcertBookingSystemTests.swift
+//  ConcertBookingSystemTests
+//
+//  Created by 牙后慧 on 2025/5/8.
+//
+
+import Testing
+@testable import ConcertBookingSystem
+
+struct ConcertBookingSystemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
