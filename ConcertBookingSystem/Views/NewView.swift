679aea29602cbbb8ebfb95c73e9510401359dda8
TriPham1312 <phcongtri93@gmail.com>
2025-05-08 15:16:06 +1000

    create new view

---
+++ ConcertBookingSystem/Views/NewView.swift
@@ -0,0 +1,18 @@
+//
+//  NewView.swift
+//  ConcertBookingSystem
+//
+//  Created by Pham Cong Tri on 8/5/25.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewView()
+}
